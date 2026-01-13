578303ea481a9ade9ddecffb2175724c9f6cfd5b
Cenk Kılıç <cenk@kilic.dev>
2026-01-13 22:27:54 +0100

    ci: update pipelien configuration

--- version.go
+++ version.go
@@ -4,4 +4,4 @@ const CLI_NAME = "docker-seafile-cli"
 
 const DESCRIPTION = "Initiates the Seafile Client Daemon that will run in this container."
 
-const VERSION = "latest"
+var VERSION = "latest"
